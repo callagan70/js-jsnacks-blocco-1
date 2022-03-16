ddf65f24ede1b6dca29de43e4ed4386138b3b1a6
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-16 11:07:44 +0000

    Fatto

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ let primo = prompt ("Dammi il primo numero")
 let secondo = prompt ("Dammi il secondo numero")
 
 if (primo > secondo) {
-    document.getElementById(fuori).innerHTML = "Il primo numero è maggiore del secondo";}
+    document.getElementById("fuori").innerHTML = "Il primo numero è maggiore del secondo";
+  }
 else{
-    document.getElementById(fuori).innerHTML = "Il secondo è maggiore del primo";}
-
-  document.getElementById(fuori).innerHTML = "Prova"
+    document.getElementById("fuori").innerHTML = "Il secondo è maggiore del primo";
+  }
